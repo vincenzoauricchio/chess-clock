4c88dc6ab7c767e5b876079b594be06da312f270
vincenzo.auricchio <vincenzo.auricchio@check24.de>
2025-11-15 11:14:53 +0100

    Remove files.

--- firmware/.pio/build/esp32-s3/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32-s3/FrameworkArduino/cbuf.cpp.o: \
- /Users/checkito744/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- /Users/checkito744/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
--- firmware/.pio/build/esp32-s3/FrameworkArduino/firmware_msc_fat.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32-s3/FrameworkArduino/firmware_msc_fat.c.o: \
- /Users/checkito744/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
- /Users/checkito744/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h
